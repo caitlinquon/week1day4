2d95daa00122f059a7df698f5a80317bc79f142c
Caitlin Quon <caitlin_quon@hotmail.com>
2017-06-01 19:32:16 +0000

    refactored waldo code

--- callback_waldo.js
+++ callback_waldo.js
@@ -1,12 +1,12 @@
 
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
-    }
-  }
+  arr.forEach(function(item, index){
+    if (item === "Waldo")
+    found(index);
+  });
 }
+
 
 function actionWhenFound(index) {
   console.log("Found him at index " + index);
